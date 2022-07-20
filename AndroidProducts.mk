0e782520f3194ace76deb7a62fd60a24b9af02e5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-20 11:50:34 +0200

    MX2: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: alps Model: MX2 Script version: 2.3.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_MX2.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_MX2-user \
+    omni_MX2-userdebug \
+    omni_MX2-eng
